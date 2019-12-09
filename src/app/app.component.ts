87ec42bf1b61827c48ec50fc565ece48cd172b47
Harsh Yadav <harsh.yadav@irissoftware.com>
2019-12-09 16:41:33 +0530

    updated files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,27 +11,34 @@ import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  implements OnInit{
+export class AppComponent implements OnInit {
   constructor(
     private _snackBar: MatSnackBar,
     private _FileSaverService: FileSaverService,
     private fb: FormBuilder
   ) { }
-  myForm:FormGroup;
+  myForm: FormGroup;
 
   selectedDate = null;
-  datePickerValue = null;
+  datePickerValue = new Date();
   defaultScore = null;
   chats = ChatsJson;
+  addCategoryValue = '';
+  addSubCategoryValue = { "Sentinal": "", "GUP": "" };
 
   allChats = this.chats;
   dropdownMenu = [
-    { value: 'sentinal', viewValue: 'Sentinal' },
+    { value: 'sentinal', viewValue: 'Sentinel' },
     { value: 'gup', viewValue: 'GUP' },
     { value: 'san-validity', viewValue: 'San validity' },
     { value: 'san-engagement', viewValue: 'San engagement' },
     { value: 'greetings', viewValue: 'Greetings' }
   ];
+
+  addCategory: any = [{ "categoryName": "Sentinal", "subCategory": ["Sentinol", "Sentinql", "Sentinwl", "Sentinrl"] },
+  { "categoryName": "GUP", "subCategory": ["GQP", "GWP", "GEP", "GRP"] }];
+  // {"categoryName":"San validity"}   ,
+  //    {"categoryName":"San engagement"} ,  {"categoryName":"Greetings"} , {"categoryName":"Greeting1"} ,  {"categoryName":"Greeting2"}];
 
   scores = [
     { value: 'p', viewValue: 'P' },
@@ -50,10 +57,11 @@ export class AppComponent  implements OnInit{
     url: 'https://app.powerbi.com/reportEmbed?reportId=92edb2bb-4709-48a0-ae9f-c52ab713b4be&autoAuth=true&ctid=54f1c50c-e960-4359-94fa-e790e17dcfb4&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWluZGlhLWNlbnRyYWwtYS1wcmltYXJ5LXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9'
   };
 
+
   // azureDashboardLink = 'https://portal.azure.com/#@ssaadminssainfosystems.onmicrosoft.com/dashboard/arm/subscriptions/401e0ead-f474-4d6a-a107-b970d52fd8cc/resourcegroups/ia_resourcdgroup/providers/microsoft.portal/dashboards/f358923d-747c-4eff-a0a8-252356c6b8a7-dashboard';
   azureDashboardLink = 'http://stackoverflow.com';
 
-  ngOnInit(){
+  ngOnInit() {
     this.myForm = this.fb.group({
       nOfIterations: '',
       nOfTopWords: '',
@@ -61,11 +69,12 @@ export class AppComponent  implements OnInit{
       seedConfidence: '',
       minSentenceLength: '',
       cBelConfScore: '',
-      date:''
+      date: ''
     });
 
-    let selfLearningData = JSON.parse(localStorage.getItem("data")); 
-    if(selfLearningData){
+    let selfLearningData = JSON.parse(localStorage.getItem("data"));
+    let addCategoryData = JSON.parse(localStorage.getItem('addCategoryData'));
+    if (selfLearningData) {
       this.myForm.controls['nOfIterations'].setValue(selfLearningData['nOfIterations']);
       this.myForm.controls['nOfTopWords'].setValue(selfLearningData['nOfTopWords']);
       this.myForm.controls['refresh'].setValue(selfLearningData['refresh']);
@@ -74,11 +83,54 @@ export class AppComponent  implements OnInit{
       this.myForm.controls['cBelConfScore'].setValue(selfLearningData['cBelConfScore']);
       this.myForm.controls['date'].setValue(selfLearningData['date']);
     }
-    console.log("selfLearningData",selfLearningData);
+
+    if (addCategoryData) {
+      this.addCategory = JSON.parse(JSON.stringify(addCategoryData));
+    }
+
+
+    console.log("selfLearningData", selfLearningData);
   }
 
-  onSubmit(){
-    localStorage.setItem("data",JSON.stringify(this.myForm.value));
+  addNote(addCategoryValue) {
+    let obj = { "categoryName": addCategoryValue, "subCategory": [] };
+    this.addCategory.push(obj);
+    console.log(" this.addCategory", this.addCategory);
+  }
+
+  saveCategory() {
+    localStorage.setItem("addCategoryData", JSON.stringify(this.addCategory));
+  }
+
+  onSubmit() {
+    localStorage.setItem("data", JSON.stringify(this.myForm.value));
+  }
+
+  deleteCategory(categoryName) {
+    for (let i = 0; i < this.addCategory.length; i++) {
+      if (this.addCategory[i]['categoryName'] == categoryName) {
+        this.addCategory.splice(i, 1);
+      }
+    }
+  }
+
+  deleteSubCategory(categoryName, categorySubName) {
+    for (let i = 0; i < this.addCategory.length; i++) {
+      if (this.addCategory[i]['categoryName'] == categoryName) {
+        let subCatIndex = this.addCategory[i].subCategory.indexOf(categorySubName);
+        this.addCategory[i].subCategory.splice(subCatIndex, 1);
+      }
+    }
+  }
+
+
+  addSubCategory(categoryName, categorySubValue) {
+    for (let i = 0; i < this.addCategory.length; i++) {
+      if (this.addCategory[i]['categoryName'] == categoryName) {
+        this.addCategory[i].subCategory.push(categorySubValue);
+      }
+    }
+    console.log("this.addCategory", this.addCategory);
   }
 
 
